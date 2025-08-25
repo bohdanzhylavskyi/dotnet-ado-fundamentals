39a4116be72a37b072d69116a143ff53a9ff8f14
bohdan zhylavskyi <zhylavskyibohdan@gmail.com>
2025-08-25 16:34:50 +0300

    initial

---
+++ ADO/ADO/CreateProduct.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[CreateProduct]
+	@Name varchar(20),
+	@Description varchar(20),
+	@Weight decimal(3, 2),
+	@Height decimal(3, 2),
+	@Width decimal(3, 2),
+	@Length decimal(3, 2)
+AS
+	INSERT INTO Products (Name, Description, Weight, Height, Width, Length)
+	VALUES (@Name, @Description, @Weight, @Height, @Width, @Length);
+RETURN 0
---
+++ ADO/ADO/Products.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NCHAR(20) NOT NULL, 
+    [Description] NCHAR(20) NOT NULL, 
+    [Weight] DECIMAL(3, 2) NOT NULL, 
+    [Height] DECIMAL(3, 2) NOT NULL, 
+    [Width] DECIMAL(3, 2) NOT NULL, 
+    [Length] DECIMAL(3, 2) NOT NULL
+)
